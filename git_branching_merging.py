d2bb87683d6f06864e7ad56dedfef14507444728
christyhernandez404 <christyhernandez404@gmail.com>
2024-07-09 15:18:22 -0400

    initial commit

---
+++ git_branching_merging.py
@@ -0,0 +1,2 @@
+#they create a branch, and add new features to it. Then they merge the feature branch into the main branch of the application
+#we'll make a main page, create some stuff in the main page
